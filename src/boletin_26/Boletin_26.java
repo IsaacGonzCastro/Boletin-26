b2f7c63ab4051fce292cbe29c3ca19b9af081449
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-04-20 11:34:14 +0200

    Boletin26

---
+++ src/boletin_26/Boletin_26.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_26;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin_26 {
+
+    
+    public static void main(String[] args) {
+         Bonoloto bono= new Bonoloto(); 
+         
+         bono.setVisible(true);
+    }
+      
+}
